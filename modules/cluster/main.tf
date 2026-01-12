22edc997fc0e450d2da9689746212ce071479a2e
Joachim Desroches <jodesroches@pictet.ch>
2026-01-12 12:21:50 +0100

    wip: fix network

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -11,7 +11,11 @@ resource "aws_launch_template" "webserver" {
   image_id      = var.ami_id
   instance_type = var.instance_type
   key_name      = var.ssh_key_name
-  # vpc_security_group_ids = var.vpc_security_group_ids
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = var.vpc_security_group_ids
+  }
 
   user_data = base64encode(var.rendered_user_data)
 }
