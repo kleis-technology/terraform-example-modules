6853ce811aacb33f1c0719d93e56da603a5abd0c
Joachim Desroches <jodesroches@pictet.ch>
2026-01-12 20:14:20 +0100

    Fix golang-petname call

--- modules/webapp/scripts/user-data.sh
+++ modules/webapp/scripts/user-data.sh
@@ -7,5 +7,5 @@ apt -y install nginx golang-petname
 cat > /var/www/html/index.html <<EOF
 <h1>Hello, World</h1>
 <p>My name is <q>${server_name}!</q></p>
-<p>My local name is <q>$(petname -w 4)</q>!</p>
+<p>My local name is <q>$(golang-petname -words 4)</q>!</p>
 EOF
